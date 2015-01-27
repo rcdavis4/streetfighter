5d4c909ffc1ff10877292da3fb6067a382de2a9e
rcdavis4 <rcdavis4@gmail.com>
2015-01-27 00:39:18 -0800

    added final animations and sound

--- js/app.js
+++ js/app.js
@@ -26,11 +26,32 @@ $(document).ready(function() {
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
-  });
+  })
+  $(document).keydown(function(event) {
+    if(event.which == 88) {
+      $('.ryu-still, .ryu-ready, .ryu-throwing').hide();
+      $('.ryu-cool').show();
+      playMusic();
+      $('.main').css('backgroundImage', 'url("../img/skyline.jpg")');
+    }
+  }).keyup(function() {
+    if(event.which == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+      $('#suave-music')[0].pause();
+      $('#suave-music')[0].load();
+      $('.main').css('backgroundImage', 'url("../img/anime.png")');
+    }
+  })
 });
 
 function playHadouken() {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
+}
+
+function playMusic() {
+  $('#suave-music')[0].volume = 0.5;
+  $('#suave-music')[0].play();
 }
