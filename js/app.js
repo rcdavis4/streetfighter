a66902fc4b17ce0bd8d61fb74f6fdfce3380c1ab
rcdavis4 <rcdavis4@gmail.com>
2015-01-27 00:52:57 -0800

    commenting code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $('.ryu')
+  // reveals ready stance when hovering near image
   .mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
+  // returns ryu to still when cursor leaves ryu area
   .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
@@ -11,9 +13,10 @@ $(document).ready(function() {
   .mousedown(function() {
     // hadouken sound
     playHadouken();
+    // reveals ryu throwing stance
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    // hadouken animation
+    // hadouken fireball animation
     $('.hadouken').finish().show().animate(
       {'left': '500px'},
       600,
@@ -23,28 +26,37 @@ $(document).ready(function() {
       }
     );
   })
+  // returns to ready stance when release click
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   })
+
+  // reveal of cool ryu when letter x is pressed
   $(document).keydown(function(event) {
     if(event.which == 88) {
       $('.ryu-still, .ryu-ready, .ryu-throwing').hide();
       $('.ryu-cool').show();
+      // cue suave music
       playMusic();
+      // replaces background image with cityscape image
       $('.main').css('backgroundImage', 'url("../img/skyline.jpg")');
     }
+  // function for when x button is released
   }).keyup(function() {
     if(event.which == 88) {
       $('.ryu-cool').hide();
       $('.ryu-still').show();
+      // stops music and loads again
       $('#suave-music')[0].pause();
       $('#suave-music')[0].load();
+      // background image returns to original
       $('.main').css('backgroundImage', 'url("../img/anime.png")');
     }
   })
 });
 
+// music and sound functions
 function playHadouken() {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
